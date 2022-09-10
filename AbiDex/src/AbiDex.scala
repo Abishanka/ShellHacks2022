a16ed8b9fbcb05bc1d50bd98904bc200928a2fdf
Tesla <tesla.radulovic@proton.me>
2022-09-10 19:45:14 -0400

    manual csv loading, and initial query database

--- AbiDex/src/AbiDex.scala
+++ AbiDex/src/AbiDex.scala
@@ -2,7 +2,7 @@ import com.nefariouszhen.trie.BurstTrie
 import java.io._
 import zamblauskas.csv.parser._
 import scala.io.Source
-
+import zamblauskas.csv.parser.util.CsvReaderUtil._
 
 case class Security(
     security_id:String,
@@ -17,27 +17,94 @@ case class Security(
     spn:Option[String])
 
 object Main extends App {
-    val suffixMap = BurstTrie.newSuffixMap[Int]()
-    suffixMap.put("banana",5)
-    suffixMap.put("ananas",56)
-    suffixMap.put("ban",566)
-    for (result <- suffixMap.query("ananas")) println(result)
+    val suffixMaps = Array.tabulate(11)( (i) => BurstTrie.newSuffixMap[ Int ]())
 
     //val baos = new ByteArrayOutputStream(1024)
     //val o = new ObjectOutputStream(baos)
     //o.writeObject(suffixMap)
 
-    val filename = "test.csv"
-    val fileContents = Source.fromFile(filename).getLines.mkString("\n")
-
-    val results = Parser.parse[Security](fileContents)
+    val filename = "Securities - Schonfeld ShellHacks.csv"
+    val fileSource = Source.fromFile(filename).getLines.toList
+    val header = fileSource.head.split(",")
+    val fileContents:Array[String] = fileSource.tail.take(1000).toArray
+    val data =
+        for{
+           line <- fileContents
+        }yield for {
+          field <- line.split(",")
+        }yield field match {
+                case "" => None
+                case s => Some(s)
+            }
     
+    
+    for{
+        i <- 0 to data.length - 1
+        j <- 0 to suffixMaps.length - 1
+        if(j < data(i).length)
+        datum <- data(i)(j)
+    }{  
+        if( i % 10000 == 0 && j == 0) println ( (1.0*i) / data.length )
+        suffixMaps(j).put( datum, i )
+    }
+    import scala.collection.mutable
+    def collect[K,V](seq:Seq[(K,V)]):mutable.HashMap[K,List[V]] = {
+        var out = new mutable.HashMap[K,List[V]]()
+        for( (k,v) <- seq ){
+            out.get(k) match {
+                case None => out += k -> (v :: Nil)
+                case Some(list) => out += k -> (v :: list)
+            }
+        }
+        out
+    }
+
+    def query(q:String) = {
+        val exactMatches = 
+            collect(for{
+                i <- 0 to header.length - 1
+                security_id <- suffixMaps(i).query(q)
+                //need to add test for this, might be bug here
+                street_id <- data(security_id)(i)
+                if(street_id == q)
+            }yield {
+                //println(street_id,q,street_id)
+                (security_id,i)
+            })
+        val partialMatches = 
+            collect(for{
+                i <- 0 to header.length - 1
+                security_id <- suffixMaps(i).query(q)
+                //need to add test for this, might be bug here
+                street_id <- data(security_id)(i)
+                if(street_id != q)
+            }yield {
+                //println(street_id,q,street_id)
+                (security_id,i)
+            })
+        (exactMatches,partialMatches)
+    }
+
+    //for( security <- suffixMaps(6).query("19N3N") ) println( data(security).mkString(",") )
+    println(query("19N3N"))
+    println(header.mkString(","))
+
+    val sc = new java.util.Scanner(System.in)
+    val s = sc.nextLine()
+    
+/*
+    val results = Parser.parse[Security](fileContents)
+    println(results)
+    println("done parsing")
     for{
         good_results <- results.right
         result <- good_results
+        bbg <- result.bbg
     }{
-        println(result)
+        
+        suffixMap.put(bbg,result)
     }
+    for( security <- suffixMap.query("19N3N") )println(security)*/
 
     
 
