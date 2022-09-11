3574f3d9494fbabc77205e4189822768ab2a1f06
visborne <visborne@proton.me>
2022-09-11 09:18:55 -0400

    ready for experiment

--- AbiDex/test/src/Test.scala
+++ AbiDex/test/src/Test.scala
@@ -5,7 +5,7 @@ object Test extends App {
 
     AbiDex.init(dirty = true,dirty_count=1000)
 
-
+/*
     AbiDex.internal_query("BBG001FC63F5")
     for( result <- AbiDex.query("BBG001FC63F") ){
         println(result.mkString(","))
@@ -13,5 +13,5 @@ object Test extends App {
 
     println( AbiDex.queryJSONString("BBG001",count = 10) )
     val sc = new java.util.Scanner(System.in)
-    val s = sc.nextLine()
+    val s = sc.nextLine()*/
 }
